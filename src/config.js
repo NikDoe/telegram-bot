a1085033b084d01cb4074385febc559fdc027226
Nikita <80026327+NikDoe@users.noreply.github.com>
2022-01-19 13:38:31 +0300

    add local url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: '5085440473:AAGvp19BHV9gns7_HRhtRKtpRT-KKDRdzsc',
-    DB_URL: process.env.MONGODB_URL || "mongodb+srv://nikdoe:<password>@cluster0.pivwl.mongodb.net/test",
+    DB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/dima-kotik-data-base',
 }
