d687703950f9193822b952f7580a6c1d0b40d643
Nikita <80026327+NikDoe@users.noreply.github.com>
2022-01-19 12:59:13 +0300

    change TOKEN

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN: '5085440473:AAGmA5mzGjk5hL1lc3fXgfMMY_i4Xyf-RCg',
+    TOKEN: '5085440473:AAGvp19BHV9gns7_HRhtRKtpRT-KKDRdzsc',
     DB_URL: 'mongodb://localhost:27017/dima-kotik-data-base',
 }
